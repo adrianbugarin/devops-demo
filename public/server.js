f764332aaf22fdbf226e32dc4a80dc760598df1f
Adrian_Bugarin <adrianbugarin3@gmail.com>
2021-11-04 09:19:45 -0600

    commit-m

--- public/server.js
+++ public/server.js
@@ -7,7 +7,7 @@ app.use(express.json())
 const students = [`Adrian`, `Spencer`, `Norman`]
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/index.html'))
+    res.sendFile(path.join(__dirname, './index.html'))
 })
 
 
