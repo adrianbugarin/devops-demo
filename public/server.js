cfb4316f03a71cb978765be6a99d19ee66fec038
Adrian_Bugarin <adrianbugarin3@gmail.com>
2021-11-04 09:13:54 -0600

    initalcommit

---
+++ public/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express
+const path = require(`path`)
+
+app.use(express.json())
+
+const students = [`Adrian`, `Spencer`, `Norman`]
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/index.html'))
+})
+
+
+
+
+
+const port = process.env.PORT || 5050
+
+app.listen(port, () => console.log(`port runnning on ${port}`))
