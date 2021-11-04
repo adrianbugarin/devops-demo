e24e62c75b455e7793a1e9119f039efe15ca440f
Adrian_Bugarin <adrianbugarin3@gmail.com>
2021-11-04 10:04:28 -0600

    commit

--- public/server.js
+++ public/server.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const app = express()
 const path = require(`path`)
-const Rollbar = require('rollbar')
+
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: '9f64ede3ca3f4cdd92cee3464b403d98',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
 
-// record a geric message and send it to Rollbar
-// rollbar.log("Hello world!");
 
 app.use(express.json())
 
